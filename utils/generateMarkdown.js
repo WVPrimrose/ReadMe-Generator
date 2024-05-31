4564c8d6675c04e258bf4e9f25affd5544131159
Katie Buchholz <kzbindy@hotmail.com>
2024-05-30 20:14:58 -0400

    re-did how I wanted to return my README file instead of having the text running straight through

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,10 +13,29 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Table of Contents
+  [Description](#description)
+  [Installation](#installation)
+  [Usage](#usage)
+  [Licenses](#licenses)
+  [Contributions](#constributions)
+  [Tests](#tests)
+  [Questions](#questions)
+
+  ## Description
   #${data.description}
   #${data.contents}
+  ## Installation
   #${data.installation}
+  ## Usage
+  #${data.usage}
+  ## Licenses
+  #${data.licenses}
+  ## Constributions
   #${data.constributions}
+  ## Tests
+  #${data.test}
+  ##Questions
   #${data.github}
   #${data.email}
 `;
